b6ac4b695e02301035e79db73277f0effc494ca6
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-11-23 18:53:30 +0000

    application.properties

--- springbootdemo/src/main/java/com/tcs/springbootdemo/SpringbootdemoApplication.java
+++ springbootdemo/src/main/java/com/tcs/springbootdemo/SpringbootdemoApplication.java
@@ -1,13 +1,35 @@
 package com.tcs.springbootdemo;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+// 1. @SpringBootConfiguration ====@Configuration
+
+//2. @EnableAutoConfiguration
+//here it enables spring boot to auto-configure the application context.
+// it automaticaly creates and register beans based on both the included jar files in the classpath & beans defined by us.
+// Spring MVC ===> spring web jar and tomcat server plugin.
+// wehn spring web starter=== > it will include the tomcat server mvc based conf.g
+// exculde names autoconfig we want to skipp specify that list to @enableautoconfiguration
+
+//3. @ComponentScan
+
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdemoApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootdemoApplication.class, args);
+		
+		String [] beanNames= applicationContext.getBeanDefinitionNames();
+		
+		Arrays.sort(beanNames);
+		
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 	}
 
 }
