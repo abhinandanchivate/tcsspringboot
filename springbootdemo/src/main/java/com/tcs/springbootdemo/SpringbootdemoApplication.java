b94e8a8d319bcc8ca93ba3872ce221b931837b59
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-11-23 19:32:09 +0000

    main

--- springbootdemo/src/main/java/com/tcs/springbootdemo/SpringbootdemoApplication.java
+++ springbootdemo/src/main/java/com/tcs/springbootdemo/SpringbootdemoApplication.java
@@ -1,10 +1,14 @@
 package com.tcs.springbootdemo;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import com.tcs.springbootdemo.model.Product;
+import com.tcs.springbootdemo.service.ProductService;
 
 @SpringBootApplication
 // 1. @SpringBootConfiguration ====@Configuration
@@ -21,15 +25,44 @@ import org.springframework.context.ApplicationContext;
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(SpringbootdemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringbootdemoApplication.class, args);
 		
-		String [] beanNames= applicationContext.getBeanDefinitionNames();
+//		String [] beanNames= applicationContext.getBeanDefinitionNames();
+//		
+//		Arrays.sort(beanNames);
+//		
+//		for (String beanName : beanNames) {
+//			System.out.println(beanName);
+//		}
 		
-		Arrays.sort(beanNames);
 		
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
+		ProductService productService = context.getBean(ProductService.class);
+		ProductService productService2 = context.getBean(ProductService.class);
+		Product product = new Product(5, "laptop modified", "i5 10th gen", 123.0f, "mac laptop");
+		String result = productService.createProduct(product);
+		System.out.println(result);
+		System.out.println(productService.equals(productService2));
+		System.out.println(productService == productService2);
+		
+		Optional<List<Product>> optional = productService.getProductsByCategory("mac laptop");
+		
+		if(optional.isPresent()) {
+			List<Product> products = optional.get();
+			products.forEach(p->{
+				System.out.println(p);
+			});
 		}
+		else {
+			System.out.println("problem");
+		}
+//		
+//		if(productService.getProductById(5).isPresent()) {
+//			System.out.println("product exists ");
+//		}
+//		else {
+//			System.out.println("not available");
+//		}
+		//context.close();
 	}
 
 }
