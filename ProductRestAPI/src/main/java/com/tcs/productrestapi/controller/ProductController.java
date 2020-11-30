fca38f851e1bd0d7c7eca186809f39f15b23a2c9
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-11-30 17:27:34 +0000

    putmapping

--- ProductRestAPI/src/main/java/com/tcs/productrestapi/controller/ProductController.java
+++ ProductRestAPI/src/main/java/com/tcs/productrestapi/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,4 +66,19 @@ public class ProductController {
 		
 		return hashMap;
 	}
+	
+	
+	@PutMapping("/{id}")
+	
+	public ResponseEntity<Product> updateProduct(@PathVariable("id") Integer id,
+			@Valid @RequestBody Product product ) throws ResourceNotFoundException {
+		Product product2 = productService.getProductById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Product not found"));
+		product.setProductId(id);
+		Product product3 =productService.createProduct(product);
+		
+		return ResponseEntity.ok(product3);
+	}
+	
+	
 }
