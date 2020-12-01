c6bfe0f38c8a2ab45717758aef13bb618917d9f6
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-12-01 15:56:08 +0000

    repo added

---
+++ authDemo/src/main/java/com/tcs/authdemo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tcs.authdemo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tcs.authdemo.model.User;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
