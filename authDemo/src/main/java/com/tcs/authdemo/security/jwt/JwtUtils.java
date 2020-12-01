a6a017f7576d4a6a6fd740a6826f7bc9b3c3d810
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-12-01 18:46:41 +0000

    userdetails impl

--- authDemo/src/main/java/com/tcs/authdemo/security/jwt/JwtUtils.java
+++ authDemo/src/main/java/com/tcs/authdemo/security/jwt/JwtUtils.java
@@ -1,7 +1,28 @@
 package com.tcs.authdemo.security.jwt;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class JwtUtils {
 
+	
+	public String generateJwtToken(Authentication authentication) {
+		// userName
+		
+		authentication.getPrincipal();
+		// do we need to build token?
+		/// do we need current time to provide IAT?
+		// do we need exp time?
+		// alog for generating token
+		// compress/compact the token
+	}
+	public String getUserNameFromJwtToken(String token) {
+		
+	}
+	
+	
+	public boolean validateJwtToken(String authToken) {
+		
+	}
 }
