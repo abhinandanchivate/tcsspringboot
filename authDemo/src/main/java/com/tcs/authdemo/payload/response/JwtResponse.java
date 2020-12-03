fba55ea195a55161955510c739de3bd7fd3b2739
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-12-03 17:11:06 +0000

    signin

--- authDemo/src/main/java/com/tcs/authdemo/payload/response/JwtResponse.java
+++ authDemo/src/main/java/com/tcs/authdemo/payload/response/JwtResponse.java
@@ -6,10 +6,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
-@AllArgsConstructor
+
 @NoArgsConstructor
 public class JwtResponse {
-	
+	private String email;
+	public JwtResponse(String token,long id, String email, String username, List<String> roles) {
+		super();
+		this.token = token;
+		this.email = email;
+		this.id = id;
+		this.username = username;
+		this.roles = roles;
+	}
 	private String token;
 	private String type = "Bearer";
 	private long id;
